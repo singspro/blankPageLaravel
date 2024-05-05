7ef2d3f36afa91469d6248eb00b87e7c12effcb1
singspro <singgih.singspro@gmail.com>
2024-05-05 15:58:59 +0700

    mengganti route get ke match post or get pada api

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/hookmessage',[whatsappApi::class,'getMessage']);
+Route::match(['get', 'post'],'/hookmessage',[whatsappApi::class,'getMessage']); 
